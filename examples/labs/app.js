6eaa5ef0eac09dca23013aeec7915422e48c1b45
Shaikh Farhan <98farhan94@gmail.com>
2020-04-26 23:29:12 +0530

    Revert

--- examples/labs/app.js
+++ examples/labs/app.js
@@ -449,7 +449,7 @@ function updateMapPosition(ev) {
     return;
 
   // set google map zoom level:
-  gmap.setZoom(Math.log2(ammap.zoomLevel) + 4);
+  gmap.setZoom(Math.log2(ammap.zoomLevel) + 5);
 
   // sync position
   gmap.setCenter( {
