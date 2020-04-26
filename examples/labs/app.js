98976aa4c25872e6d93a2300eccb5d45cacba826
Shaikh Farhan <98farhan94@gmail.com>
2020-04-26 23:25:46 +0530

    Scroll Test

--- examples/labs/app.js
+++ examples/labs/app.js
@@ -449,7 +449,7 @@ function updateMapPosition(ev) {
     return;
 
   // set google map zoom level:
-  gmap.setZoom(Math.log2(ammap.zoomLevel) + 5);
+  gmap.setZoom(Math.log2(ammap.zoomLevel) + 1);
 
   // sync position
   gmap.setCenter( {
